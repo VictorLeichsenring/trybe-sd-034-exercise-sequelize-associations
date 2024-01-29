d7e2a6e3257f18c73db21547a4b0e5e0f411254e
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-01-29 00:47:56 -0300

    Criado model Patient

---
+++ src/models/Patient.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const Patient = sequelize.define(
+  'Patient',
+  {
+    patientId: { type: DataTypes.INTEGER, primaryKey: true, field: 'patient_id' },
+    fullname: DataTypes.STRING,
+    planId: { type: DataTypes.INTEGER, foreignKey: true },
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  },
+);
+
+  return Patient;
+};
